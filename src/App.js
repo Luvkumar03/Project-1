8102597ddab161a6f5b829152f51395432623e0d
LawKumar <luv19641@gmail.com>
2023-04-13 21:54:57 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import {Routes,Route } from 'react-router-dom';
+import './App.css';
+import Navi from './Navi';
+import Login from './Login';
+import Register from './Register';
+import AboutUs from './AboutUs';
+
+function App() {
+  return (
+  <div className="App"> 
+   <Routes>
+     <Route path="/" element = {<Navi/>}/>
+     <Route exact path="/AboutUs" element = {<AboutUs/>}/>
+     <Route exact path="/Login" element = {<Login/>}/>
+     <Route exact path="/Register" element = {<Register/>}/>
+     <Route exact path="/SignUp" element = {<Register/>}/>
+   </Routes>
+  </div>
+  );
+}
+export default App;
